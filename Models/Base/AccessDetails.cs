310bf400135799c325253f39ad7dd8f2e2bc9062
bhuvaneswari <bhuvaneswari.nagarajan@syncfusion.com>
2019-12-31 10:58:44 +0530

    config(EJ2-34938): Update the development branch in Core

---
+++ Models/Base/AccessDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if EJ2_DNX
+using System.Web;
+#endif
+
+namespace Syncfusion.EJ2.FileManager.Base
+{
+    public class AccessDetails
+    {
+        public string Role { get; set; }
+        public IEnumerable<AccessRule> AccessRules { get; set; }
+    }
+
+    public class AccessRule
+    {
+        public Permission Copy { get; set; }
+        public Permission Download { get; set; }
+        public Permission Write { get; set; }
+        public string Path { get; set; }
+        public Permission Read { get; set; }
+        public string Role { get; set; }
+        public Permission WriteContents { get; set; }
+        public Permission Upload { get; set; }
+        public bool IsFile { get; set; }
+        public string Message { get; set; }
+    }
+    public enum Permission
+    {
+        Allow,
+        Deny
+    }
+}
