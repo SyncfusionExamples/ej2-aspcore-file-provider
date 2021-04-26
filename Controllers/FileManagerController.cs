6b20986e538d66356bb4e5faa96eb939c8b90f21
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-04-26 09:55:09 +0600

    Changed deprecated hosting environment class

--- Controllers/FileManagerController.cs
+++ Controllers/FileManagerController.cs
@@ -1,4 +1,4 @@
-﻿using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
+using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 using Syncfusion.EJ2.FileManager.Base;
 
 namespace EJ2APIServices.Controllers
@@ -16,78 +18,75 @@ namespace EJ2APIServices.Controllers
     [EnableCors("AllowAllOrigins")]
     public class FileManagerController : Controller
     {
-        public PhysicalFileProvider operation;
-        public string basePath;
-        string root = "wwwroot\\Files";
-        public FileManagerController(IHostingEnvironment hostingEnvironment)
+        private readonly PhysicalFileProvider operation;
+        private readonly IWebHostEnvironment hostingEnv;
+        private const string Root = "wwwroot\\Files";
+        
+        public FileManagerController(IWebHostEnvironment hostingEnvironment)
         {
-            this.basePath = hostingEnvironment.ContentRootPath;
-            this.operation = new PhysicalFileProvider();
-            this.operation.RootFolder(this.basePath + "\\" + this.root);
+            hostingEnv = hostingEnvironment;
+            var basePath = hostingEnvironment.ContentRootPath;
+            operation = new PhysicalFileProvider();
+            operation.RootFolder(basePath + "\\" + Root);
         }
+        
         [Route("FileOperations")]
         public object FileOperations([FromBody] FileManagerDirectoryContent args)
         {
-            if (args.Action == "delete" || args.Action == "rename")
+            if (args.Action is "delete" or "rename")
             {
                 if ((args.TargetPath == null) && (args.Path == ""))
                 {
-                    FileManagerResponse response = new FileManagerResponse();
-                    response.Error = new ErrorDetails { Code = "401", Message = "Restricted to modify the root folder." };
-                    return this.operation.ToCamelCase(response);
+                    FileManagerResponse response = new()
+                    {
+                        Error = new ErrorDetails { Code = "401", Message = "Restricted to modify the root folder." }
+                    };
+                    return operation.ToCamelCase(response);
                 }
             }
-            switch (args.Action)
-            {
-                case "read":
-                    // reads the file(s) or folder(s) from the given path.
-                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
-                case "delete":
-                    // deletes the selected file(s) or folder(s) from the given path.
-                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names));
-                case "copy":
-                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
-                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
-                case "move":
-                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
-                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
-                case "details":
-                    // gets the details of the selected file(s) or folder(s).
-                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
-                case "create":
-                    // creates a new folder in a given path.
-                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name));
-                case "search":
-                    // gets the list of file(s) or folder(s) from a given path based on the searched key string.
-                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
-                case "rename":
-                    // renames a file or folder.
-                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName));
-            }
-            return null;
-        }
 
+            return args.Action switch
+            {
+                "read" => operation.ToCamelCase(operation.GetFiles(args.Path, args.ShowHiddenItems)),
+                "delete" => operation.ToCamelCase(operation.Delete(args.Path, args.Names)),
+                "copy" => operation.ToCamelCase(operation.Copy(args.Path, args.TargetPath, args.Names,
+                    args.RenameFiles, args.TargetData)),
+                "move" => operation.ToCamelCase(operation.Move(args.Path, args.TargetPath, args.Names,
+                    args.RenameFiles, args.TargetData)),
+                "details" => operation.ToCamelCase(operation.Details(args.Path, args.Names, args.Data)),
+                "create" => operation.ToCamelCase(operation.Create(args.Path, args.Name)),
+                "search" => operation.ToCamelCase(operation.Search(args.Path, args.SearchString,
+                    args.ShowHiddenItems, args.CaseSensitive)),
+                "rename" => operation.ToCamelCase(operation.Rename(args.Path, args.Name, args.NewName)),
+                _ => null
+            };
+        }
+        
+        
         // uploads the file(s) into a specified path
         [Route("Upload")]
         public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action)
         {
-            FileManagerResponse uploadResponse;
-            uploadResponse = operation.Upload(path, uploadFiles, action, null);
-            if (uploadResponse.Error != null)
+            if (string.IsNullOrWhiteSpace(path))
             {
-               Response.Clear();
-               Response.ContentType = "application/json; charset=utf-8";
-               Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
-               Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+                path = "/";
             }
+
+            var uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error == null) return Content("");
+            Response.Clear();
+            Response.ContentType = "application/json; charset=utf-8";
+            Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+            Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
             return Content("");
         }
+
 
         // downloads the selected file(s) and folder(s)
         [Route("Download")]
         public IActionResult Download(string downloadInput)
         {
-            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            var args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
             return operation.Download(args.Path, args.Names, args.Data);
         }
 
@@ -95,8 +94,8 @@ namespace EJ2APIServices.Controllers
         [Route("GetImage")]
         public IActionResult GetImage(FileManagerDirectoryContent args)
         {
-            return this.operation.GetImage(args.Path, args.Id,false,null, null);
-        }       
+            return this.operation.GetImage(args.Path, args.Id, false, null, null);
+        }      
     }
 
 }
