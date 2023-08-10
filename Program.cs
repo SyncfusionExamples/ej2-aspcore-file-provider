391e92f4541390192a7b78d462e8a4cd9481a3ec
Kannan <74587271+kannans24@users.noreply.github.com>
2023-08-10 12:40:00 +0530

    Update Program.cs
    Space commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace EJ2FileManagerService
 {
-    public class Program
+    public class Program 
     {
         public static void Main(string[] args)
         {
