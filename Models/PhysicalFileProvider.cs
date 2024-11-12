94b750f78a85eb2125396ca35467669437f1df63
sivakumar <93644655+sivakumars3442@users.noreply.github.com>
2024-11-13 01:13:18 +0530

    821380: Resolved the location issue in file manager dialog.

--- Models/PhysicalFileProvider.cs
+++ Models/PhysicalFileProvider.cs
@@ -24,7 +24,7 @@ using Microsoft.Net.Http.Headers;
 
 namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
 {
-    public class PhysicalFileProvider
+    public class PhysicalFileProvider : PhysicalFileProviderBase
     {
         protected string contentRootPath;
         protected string[] allowedExtension = new string[] { "*" };
@@ -379,10 +379,12 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
                         if (previousPath == relativePath && !isVariousFolders)
                         {
                             previousPath = relativePath;
+                            fileDetails.Location = previousPath.Substring(1);
                         }
                         else
                         {
                             isVariousFolders = true;
+                            fileDetails.Location = "Various Folders";
                         }
                     }
                     fileDetails.Size = byteConversion(long.Parse(fileDetails.Size)).ToString();
@@ -1276,7 +1278,7 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
 #if EJ2_DNX
         public virtual FileManagerResponse Upload(string path, IList<System.Web.HttpPostedFileBase> uploadFiles, string action, params FileManagerDirectoryContent[] data)
 #else
-        public virtual FileManagerResponse Upload(string path, IList<IFormFile> uploadFiles, string action, long size = 0, params FileManagerDirectoryContent[] data)
+        public virtual FileManagerResponse Upload(string path, IList<IFormFile> uploadFiles, string action, long size, params FileManagerDirectoryContent[] data)
 #endif
         {
             FileManagerResponse uploadResponse = new FileManagerResponse();
@@ -1307,10 +1309,6 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
 #if EJ2_DNX
                         var name = System.IO.Path.GetFileName(file.FileName);
                         var fullName = Path.Combine((this.contentRootPath + path), name);
-                        if (Path.GetFullPath(fullName) != GetFilePath(fullName) + Path.GetFileName(fullName))
-                        {
-                            throw new UnauthorizedAccessException("Access denied for Directory-traversal");
-                        }
 #else
                         var name = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim().ToString();
                         string[] folders = name.Split('/');
@@ -1320,18 +1318,16 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
                         {
                             throw new UnauthorizedAccessException("Access denied for Directory-traversal");
                         }
-                        long fileLength = File.Exists(fullName) ? new FileInfo(fullName).Length : 0;
 #endif
+                        long fileLength = File.Exists(fullName) ? new FileInfo(fullName).Length : default;
                         if (action == "save")
                         {
-#if !EJ2_DNX
                             bool isValidChunkUpload = file.ContentType == "application/octet-stream" && (fileLength != size);
                             if (!System.IO.File.Exists(fullName) || isValidChunkUpload)
                             {
+#if !EJ2_DNX
                                 PerformUpload(file, fileLength, size, fullName);
 #else
-                            if (!System.IO.File.Exists(fullName))
-                            {
                                 file.SaveAs(fullName);
 #endif
                             }
@@ -1356,18 +1352,14 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
                         }
                         else if (action == "replace")
                         {
-#if !EJ2_DNX
                             long duplicateFileSize = new FileInfo(fullName).Length;
-                            if (System.IO.File.Exists(fullName) && (duplicateFileSize == size || file.ContentType != "application/octet-stream"))
+                            if (System.IO.File.Exists(fullName) && duplicateFileSize == size)
                             {
                                 System.IO.File.Delete(fullName);
                             }
+#if !EJ2_DNX
                             PerformUpload(file, fileLength, size, fullName);
 #else
-                            if (System.IO.File.Exists(fullName))
-                            {
-                                System.IO.File.Delete(fullName);
-                            }
                             file.SaveAs(fullName);
 #endif
                         }
@@ -1380,9 +1372,8 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
                             int fileCount = 0;
                             while (System.IO.File.Exists(newName + (fileCount > 0 ? "(" + fileCount.ToString() + ")" + Path.GetExtension(name) : Path.GetExtension(name))))
                             {
-#if !EJ2_DNX
                                 long duplicateSize = new FileInfo(newName + (fileCount > 0 ? "(" + fileCount.ToString() + ")" + Path.GetExtension(name) : Path.GetExtension(name))).Length;
-                                if (duplicateSize == size || file.ContentType != "application/octet-stream")
+                                if (duplicateSize== size)
                                 {
                                     fileCount++;
                                 }
@@ -1390,9 +1381,6 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
                                 {
                                     break;
                                 }
-#else
-                                fileCount++;
-#endif
                             }
                             newName = newName + (fileCount > 0 ? "(" + fileCount.ToString() + ")" : "") + Path.GetExtension(name);
 #if !EJ2_DNX
@@ -1426,7 +1414,6 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
             }
         }
 
-#if !EJ2_DNX
         private void PerformUpload(IFormFile file, long fileLength, long size, string name)
         {
             bool isValidChunkUpload = file.ContentType == "application/octet-stream" && (fileLength != size);
@@ -1447,8 +1434,6 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
                 }
             }
         }
-#endif
-
 #if SyncfusionFramework4_0
         public virtual void Download(string path, string[] names, params FileManagerDirectoryContent[] data)
         {
@@ -2217,6 +2202,11 @@ namespace Syncfusion.EJ2.FileManager.PhysicalFileProvider
             };
 
             return JsonSerializer.Serialize(userData, options);
+        }
+
+        FileStreamResult FileProviderBase.Download(string path, string[] names, params FileManagerDirectoryContent[] data)
+        {
+            throw new NotImplementedException();
         }
 
         private bool CheckChild(string path)
